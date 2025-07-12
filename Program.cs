e8235da94cb966a67140513b7e2d7ca616f027ab
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-07-12 20:26:25 -0300

    chore: add base project structure

---
+++ Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddStackExchangeRedisCache( options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    options.InstanceName = "EncurtadorUrl";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.Run();
