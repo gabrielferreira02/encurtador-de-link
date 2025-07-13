19fcba57332125b0a2c3235798e3bc5e178647c1
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-07-13 12:07:25 -0300

    feat(data):  add db context configuration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+using EncurtadorUrl.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EncurtadorUrl.Data;
+
+public class AppDbContext : DbContext
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public DbSet<UrlEntity> Urls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UrlEntity>(entity =>
+        {
+            entity.HasIndex(u => u.Code).IsUnique();
+            entity.HasIndex(u => u.Code).IsUnique();
+        });
+    }
+
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //     optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+    //     base.OnConfiguring(optionsBuilder);
+    // }
+}
