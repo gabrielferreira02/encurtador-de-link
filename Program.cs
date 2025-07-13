a27e126ac924c3c11f3d2d33c1437f38254ca2b0
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-07-13 12:09:04 -0300

    refactor:  register db context

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
+using EncurtadorUrl.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddStackExchangeRedisCache( options =>
+builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
     options.InstanceName = "EncurtadorUrl";
+});
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
 });
 
 var app = builder.Build();
