6fa8f3a9d0ae735bf7e18f5c3a71ed627e1c5c7d
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-07-15 13:25:40 -0300

    refactor(Data): change to internal AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EncurtadorUrl.Data;
 
-public class AppDbContext : DbContext
+internal class AppDbContext : DbContext
 {
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
